679c5baf5be2f0744511bc077d5c7b973696d053
panchalmukundak <mukunda.panchal@gmail.com>
2022-02-18 20:15:53 +0530

    code update

---
+++ binary/config.php
@@ -0,0 +1,8 @@
+<?php
+
+const MY_AWS_REGION = "ap-south-1"
+const MY_ECR_REPOSITORY = NULL
+const MY_ECS_SERVICE = NULL
+const MY_ECS_CLUSTER = NULL
+const MY_ECS_TASK_DEFINITION = NULL
+const MY_CONTAINER_NAME = NULL
